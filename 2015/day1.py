e30f355986fd0c12423fffca98852cae30466e4a
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2022-10-24 08:53:06 +0800

    2015-01

---
+++ 2015/day1.py
@@ -0,0 +1,16 @@
+def day1(data):
+    floor = 0
+    part2 = None
+    for i, char in enumerate(data):
+        if char == '(':
+            floor += 1
+        else:
+            floor -= 1
+            if floor < 0 and part2 is None:
+                part2 = i+1
+    return floor, part2
+
+if __name__ == '__main__':
+    with open('inputs/day1.txt') as file:
+        data = file.read()
+    print("Part 1: {}\nPart 2: {}".format(*day1(data)))
