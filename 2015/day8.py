394d6331843a53aa10b5a2a078df88b42eeabad1
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2022-10-25 16:22:24 +0800

    2015-08

---
+++ 2015/day8.py
@@ -0,0 +1,16 @@
+import re
+
+
+def day8(data):
+    part1 = 0
+    part2 = 0
+    for line in data.splitlines():
+        part1 += sum(len(i)-1 for i in re.findall(r'\\"|\\\\|\\x..', line)) + 2
+        part2 += line.count('\\') + line.count('"') + 2
+    return part1, part2
+
+
+if __name__ == '__main__':
+    with open('inputs/day8.txt') as file:
+        data = file.read()
+    print("Part 1: {}\nPart 2: {}".format(*day8(data)))
