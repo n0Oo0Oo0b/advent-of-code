cdc5657550ac63b59562f15215d943c88cf7719c
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2022-12-02 08:53:42 +0800

    Submit both parts at once

--- 2022/master.py
+++ 2022/master.py
@@ -8,8 +8,5 @@ for day_num in range(1, 26):
     if solution is not None:
         puzzle = Puzzle(day=day_num, year=2022)
         part1, part2 = solution.solve(puzzle.input_data)
-        puzzle.answer_a = part1
-        print(puzzle.answer_a)
-        if part2 is not None:
-            puzzle.answer_b = part2
         print(f"Day {day_num}: {part1}, {part2}")
+        puzzle.answers = part1, part2
