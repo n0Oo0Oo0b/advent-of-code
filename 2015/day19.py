642a9b70e7866a863439c81d0d71ee24ae16bc17
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-01-12 16:10:46 +0800

    2015-19

--- 2015/day19.py
+++ 2015/day19.py
@@ -2,29 +2,34 @@ from collections import defaultdict
 import re
 
 
-def _parse_chemicals(inp):
-    return re.split(r'(?=[A-Z])', inp)[1:]
-
-
-def _part1(conversions, chemical):
-    possible = set()
-    for i, element in enumerate(chemical):
-        for result in conversions[element]:
-            r = ''.join(chemical[:i] + result + chemical[i+1:])
-            possible.add(r)
-    return len(possible)
-
-
 def day19(inp):
+    # Parse input
     raw_conversions, _, chemical = inp.partition('\n\n')
-    conversions = defaultdict(list)
+    conversions = defaultdict(list)  # maps reactant to products
+    reductions = {}  # maps product to reactant
     for line in raw_conversions.splitlines():
         reactant, _, product = line.partition(' => ')
-        conversions[reactant].append(_parse_chemicals(product))
-    chemical = _parse_chemicals(chemical)
-    return _part1(conversions, chemical)
+        conversions[reactant].append(product)
+        reductions[product] = reactant
+    # Part 1
+    possible_next = set()
+    for reactant, products in conversions.items():
+        for m in re.finditer(reactant, chemical):
+            start, end = m.span()
+            for product in products:
+                possible_next.add(chemical[:start] + product + chemical[end:])
+    part1 = len(possible_next)
+    # Part 2
+    pattern = re.compile(fr'.*({"|".join(reductions)})')  # pattern to find rightmost replacement
+    part2 = 0
+    while chemical != 'e':
+        m = re.match(pattern, ''.join(chemical))
+        (start, end), group = m.span(1), m.group(1)
+        chemical = chemical[:start] + reductions[group] + chemical[end:]
+        part2 += 1
+    return part1, part2
 
 
 if __name__ == '__main__':
     from aocd import data
-    print("Part 1: {}".format(day19(data)))
+    print("Part 1: {}\nPart 2: {}".format(*day19(data)))
