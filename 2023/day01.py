aaa45ef609b3d7db60566654c89d3f08f1302ec9
Daniel Gu <bobthebuilder10492@gmail.com>
2023-12-17 21:06:33 +0800

    Cleanup earlier days

--- 2023/day01.py
+++ 2023/day01.py
@@ -1,14 +1,6 @@
-import regex as re
-import math
-import numpy as np
-from itertools import batched, product
-from util import get_ints, get_floats, data
+from util import *
 
-blocks = [i.split("\n") for i in data.split("\n\n")]
 lines = data.split("\n")
-ints = get_ints(data)
-floats = get_floats(data)
-
 
 nums = list(map(get_ints, lines))
 
--- 2023/day08.py
+++ 2023/day08.py
@@ -1,22 +1,8 @@
-import regex as re
-from math import *
-import numpy as np
-from collections import *
 from itertools import *
 from util import *
-import sympy as sp
 
 
 res = 0
-
-blocks = [i.split("\n") for i in data.split("\n\n")]
-lines = data.split("\n")
-line_ints = [get_ints(line) for line in lines]
-ints = get_ints(data)
-floats = get_floats(data)
-
-sparse_grid = SparseGrid.from_input(lines, lambda s: s if s != '.' else None)
-grid = Grid.from_input(lines)
 
 graph = {}
 
